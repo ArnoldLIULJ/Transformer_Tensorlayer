1c1215b7b92d2f277ed7028b3a519344b79fc528
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-06-29 13:35:20 +0100

    update

--- weightLightModels/LightConv.py
+++ weightLightModels/LightConv.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 import tensorlayer as tl
-from weightLightModels.DepthwiseConv import DepthwiseConv
-
+from tensorlayer.layers.convolution.depthwise_conv import DepthwiseConv2d
+from weightLightModels.normalization import WeightNorm
 class LConv(tl.models.Model):
     def __init__(self, params, name=None):
         super(LConv, self).__init__(name=name)
@@ -9,15 +9,14 @@ class LConv(tl.models.Model):
         self.H = params.H
         self.in_channels = params.filter_number//2
         # self.softmax_layer = tl.layers.Lambda(lambda x: tf.nn.softmax(x), name='lambda')
-        self.conv_Layer = DepthwiseConv(
+        self.conv_Layer = WeightNorm(DepthwiseConv2d(
                 filter_size=params.light_filter_size,
-                in_channels=self.H)
+                in_channels=self.H), in_channels=self.H)
     
     def forward(self, inputs):
 
         for i in range(self.in_channels//self.H):
             inputs_sliced = inputs[:,:,:,i*self.H:(i+1)*self.H]
-            # self.conv_Layer.W = self.softmax_layer(self.conv_Layer.W)
             outputs_sliced = self.conv_Layer(inputs_sliced)
             if i == 0:
                 output = outputs_sliced
