79074ce83c4356c25843af1fa36adbca1861f7e6
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-07-01 17:39:22 +0100

    small dataset

--- tutorial_translate.py
+++ tutorial_translate.py
@@ -146,7 +146,7 @@ if __name__ == "__main__":
   subtokenizer = tokenizer.Subtokenizer("data/data/"+VOCAB_FILE)
   params = model_params.EXAMPLE_PARAMS
   model = Transformer(params)
-  load_weights = tl.files.load_npz(name='./checkouts_tl/model.npz')
+  load_weights = tl.files.load_npz(name='./checkpoints_tl/model.npz')
   tl.files.assign_weights(load_weights, model)
   input_file = "./data/raw/dev/newstest2013.en"
   translate_file(model, subtokenizer, input_file, output_file="./output/out.de")
