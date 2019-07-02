9496ff4fc98fdf66d046d250cf59db222d480426
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-07-02 10:36:19 +0100

    update model

--- weightLightModels/LightWeightConvolution.py
+++ weightLightModels/LightWeightConvolution.py
@@ -18,20 +18,39 @@ class LightConv(tl.models.Model):
     def forward(self, inputs, cache=None):
         Batch_size = inputs.shape[0]
         original_length = inputs.shape[1]
+        
 
         if cache is not None:
-            inputs = tf.concat([cache["ids"], inputs], axis=1)
+            stored_length = cache["ids"].shape[1]
+            if (stored_length < self.params.filter_size-1):
+                inputs = tf.pad(inputs, [[0,0],[self.params.filter_size-original_length, 0],[0,0]])
+            else:
+                inputs = tf.concat([cache["ids"], inputs], axis=1)[:,-self.params.filter_size:,:]
+            # print(inputs.shape)
             cache["ids"] = inputs
         
         inputs = tf.reshape(inputs, [-1, inputs.shape[-1]])
         inputs = self.in_layer(inputs)
 
         inputs = tf.reshape(inputs, [Batch_size, -1, inputs.shape[-1]])
-        inputs = tf.pad(inputs, [[0,0],[self.params.filter_size-1, 0],[0,0]])
+        if (cache is None):
+            inputs = tf.pad(inputs, [[0,0],[self.params.filter_size-1, 0],[0,0]])
+
         inputs = self.glu_layer(inputs)
-        inputs = tf.pad(inputs, [[0,0],[self.params.light_filter_size[1]-1, 0],[0,0]])
+        
+        if cache is not None:
+            stored_length = cache["ids_"].shape[1]
+            if (stored_length < self.params.filter_size):
+                inputs = tf.pad(inputs, [[0,0],[self.params.filter_size-original_length, 0],[0,0]])
+            inputs = tf.concat([cache["ids_"], inputs], axis=1)[:,-self.params.filter_size:,:]
+            # print(inputs.shape)
+            cache["ids_"] = inputs
+
+        if (cache is None):
+            inputs = tf.pad(inputs, [[0,0],[self.params.filter_size-1, 0],[0,0]])
         # reshape inputs to [B, 1, S, H]
         inputs = tf.expand_dims(inputs, axis=1)
+        # print(inputs.shape)
         inputs = self.light_conv_layer(inputs)
 
         inputs = tf.squeeze(inputs, axis=1)
