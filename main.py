7ef9db82f451a03ac5035fb1cab491ba9b3f9575
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-07-03 11:22:53 +0100

    add tuckle

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-import tensorflow as tf
-import tensorlayer as tl
-from weightLightModels.model_params import TINY_PARAMS
-from weightLightModels.GatedLinearUnit import GLU
-from weightLightModels.LightConv import LConv
-from weightLightModels.LightWeightConvolution import LightConv
-input = tl.layers.Input([8, 1, 64], name='input')
-net = LightConv(TINY_PARAMS, name="LConv")
-net.train()
-print(net(input))
+import numpy as np
+from time import time
+from sklearn import manifold
+
+def fit_transform(X_highdimensional, n_neighbors, n_components):
+    '''
+        Performs Laplacian Eigenmaps on the given highdimensional dataset. The data needs to be a numpy array of the form (n_samples, n_features).
+        X_highdimensional:  (n_samples, n_features) matrix containing the highdimensional datapoints.
+        n_neighbors: The number of neighbours to consider for each point. Default: 12
+        Returns a tuple (X_low, time) with the lowdimensional representation and the time the execution took.
+    '''    
+    laplacian_eigenmaps = manifold.SpectralEmbedding(n_components=n_components, affinity ="nearest_neighbors", 
+                                                     random_state=None, n_neighbors=n_neighbors)
+    t0 = time()
+    X_low = laplacian_eigenmaps.fit_transform(X_highdimensional)
+    return X_low, (time() - t0)
+
+
+
+X_highdimensional = np.random.random(size=(50,100))
+print(fit_transform(X_highdimensional)[0].shape)
--- models/feedforward_layer_v2.py
+++ models/feedforward_layer_v2.py
@@ -76,3 +76,152 @@ class FeedForwardNetwork(tl.models.Model):
     output = tf.reshape(output, [batch_size, -1, output.shape[-1]])
 
     return output
+
+
+
+class TuckerDecomposition_FeedForwardNetwork(tl.layers.Layer):
+  """Fully connected feedforward network."""
+
+  def __init__(self, hidden_size, filter_size, relu_dropout, R1, R2):
+    """Initialize FeedForwardNetwork.
+
+    Args:
+      hidden_size: int, output dim of hidden layer.
+      filter_size: int, filter size for the inner (first) dense layer.
+      relu_dropout: float, dropout rate for training.
+    """
+    super(TuckerDecomposition_FeedForwardNetwork, self).__init__()
+    self.I2 = hidden_size
+    self.I1 = filter_size
+    self.R1 = R1
+    self.R2 = R2
+    self.relu_dropout = relu_dropout
+    
+    self.build(tuple())
+    self._built = True
+
+  def build(self, inputs_shape):
+    with tf.name_scope("tucker_decomposition"):
+
+        self.U1 = self._get_weights('U1', shape=(self.I1, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+        self.U2 = self._get_weights('U2', shape=(self.I2, self.R2),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R2**-0.5))
+        self.G = self._get_weights('G', shape=(self.R2, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+
+        # self.U1 = self._get_weights('U1', shape=(self.I1, self.R1),
+        #     init=tf.random_normal_initializer(mean=0., stddev=self.hidden_size**-0.5))
+        # self.U2 = self._get_weights('U1', shape=(self.I2, self.R2),
+        #     init=tf.random_normal_initializer(mean=0., stddev=self.hidden_size**-0.5))
+        # self.G = self._get_weights('G', shape=(self.R2, self.R1),
+        #     init=tf.random_normal_initializer(mean=0., stddev=self.hidden_size**-0.5))
+
+
+  def forward(self, inputs):
+    """Return outputs of the feedforward network.
+
+    Args:
+      x: tensor with shape [batch_size, length, hidden_size]
+      training: boolean, whether in training mode or not.
+
+    Returns:
+      Output of the feedforward network.
+      tensor with shape [batch_size, length, hidden_size]
+    """
+    # Retrieve dynamically known shapes
+    x = inputs
+    batch_size = tf.shape(x)[0]
+    length = tf.shape(x)[1]
+    x = tf.reshape(x, [-1, x.shape[-1]])
+    w = tf.matmul(self.U2, self.G)
+    w = tf.matmul(w, tf.transpose(self.U1))
+    output = tf.matmul(x, w)
+    output = tf.nn.relu(output)
+    output = tf.reshape(output, [batch_size, -1, output.shape[-1]])
+    if self.is_train:
+      output = tf.nn.dropout(output, rate=self.relu_dropout)
+    output = tf.reshape(output, [-1, output.shape[-1]])
+    w = tf.matmul(self.U1, tf.transpose(self.G))
+    w = tf.matmul(w, tf.transpose(self.U2))
+    output = tf.matmul(output, w)
+    # output = self.output_dense_layer(output)
+    output = tf.reshape(output, [batch_size, -1, output.shape[-1]])
+
+    return output
+
+
+
+
+
+
+class TuckerDecomposition_FeedForwardNetwork_2(tl.layers.Layer):
+  """Fully connected feedforward network."""
+
+  def __init__(self, hidden_size, filter_size, relu_dropout, R1, R2):
+    """Initialize FeedForwardNetwork.
+
+    Args:
+      hidden_size: int, output dim of hidden layer.
+      filter_size: int, filter size for the inner (first) dense layer.
+      relu_dropout: float, dropout rate for training.
+    """
+    super(TuckerDecomposition_FeedForwardNetwork_2, self).__init__()
+    self.I2 = hidden_size
+    self.I1 = filter_size
+    self.R1 = R1
+    self.R2 = R2
+    self.relu_dropout = relu_dropout
+    
+    self.build(tuple())
+    self._built = True
+
+  def build(self, inputs_shape):
+    with tf.name_scope("tucker_decomposition"):
+
+        self.U1 = self._get_weights('U1', shape=(self.I1, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+        self.U2 = self._get_weights('U2', shape=(self.I2, self.R2),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R2**-0.5))
+        self.G = self._get_weights('G', shape=(self.R2, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+
+        self.U1_ = self._get_weights('U1_', shape=(self.I1, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+        self.U2_ = self._get_weights('U2_', shape=(self.I2, self.R2),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R2**-0.5))
+        self.G_ = self._get_weights('G_', shape=(self.R2, self.R1),
+            init=tf.random_normal_initializer(mean=0., stddev=self.R1**-0.5))
+
+
+  def forward(self, inputs):
+    """Return outputs of the feedforward network.
+
+    Args:
+      x: tensor with shape [batch_size, length, hidden_size]
+      training: boolean, whether in training mode or not.
+
+    Returns:
+      Output of the feedforward network.
+      tensor with shape [batch_size, length, hidden_size]
+    """
+    # Retrieve dynamically known shapes
+    x = inputs
+    batch_size = tf.shape(x)[0]
+    length = tf.shape(x)[1]
+    x = tf.reshape(x, [-1, x.shape[-1]])
+    w = tf.matmul(self.U2, self.G)
+    w = tf.matmul(w, tf.transpose(self.U1))
+    output = tf.matmul(x, w)
+    output = tf.nn.relu(output)
+    output = tf.reshape(output, [batch_size, -1, output.shape[-1]])
+    if self.is_train:
+      output = tf.nn.dropout(output, rate=self.relu_dropout)
+    output = tf.reshape(output, [-1, output.shape[-1]])
+    w = tf.matmul(self.U1_, tf.transpose(self.G_))
+    w = tf.matmul(w, tf.transpose(self.U2_))
+    output = tf.matmul(output, w)
+    # output = self.output_dense_layer(output)
+    output = tf.reshape(output, [batch_size, -1, output.shape[-1]])
+
+    return output
