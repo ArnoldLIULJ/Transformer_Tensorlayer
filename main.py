7dafe507c9a7c481d85ce2625dda0bebee9d1b5d
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-07-08 11:00:44 +0100

    add task

--- main.py
+++ main.py
@@ -1,57 +1,128 @@
-# Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-# ==============================================================================
-"""Defines the Transformer model in TF 2.0.
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-Model paper: https://arxiv.org/pdf/1706.03762.pdf
-Transformer model code source: https://github.com/tensorflow/tensor2tensor
-"""
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+import os
+import unittest
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+import numpy as np
 import tensorflow as tf
 import tensorlayer as tl
-from models.transformer_v2 import Transformer
-from models import model_params
+from tqdm import tqdm
+from sklearn.utils import shuffle
+from models.GAN import gan_transformer as Transformer
+from models.model_params import TINY_PARAMS
+from tests.utils import CustomTestCase
+from utils import metrics
+from tensorlayer.cost import cross_entropy_seq
+from models import optimizer
+
+import time
 
 
 
 
 
-class gan_transformer(tl.models.Model):
-  def __init__(self, params, name=None):
-    super(gan_transformer, self).__init__(name=name)
-    self.genearator = Transformer(params)
-    self.discriminator = tl.layers.Dense(n_units=1, in_channels=params.vocab_size, act=tf.nn.sigmoid)
 
-  def forward(self, inputs):
-    input = inputs[0]
-    target = inputs[1]
-    length = target.shape[1]
-    fake = self.genearator(input, targets = target)
-    fake = tf.reshape(fake, [-1, fake.shape[-1]])
-    output = self.discriminator(fake)
-    output = tf.reshape(output, [-1, length, 1])
-    return fake, output
+class Model_SEQ2SEQ_Test(CustomTestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.batch_size = 16
+
+        cls.embedding_size = 32
+        cls.dec_seq_length = 5
+        cls.trainX = np.random.randint(low=2, high=50, size=(50, 10))
+        cls.trainY = np.random.randint(low=2, high=50, size=(50, 11))
+
+        cls.trainX[:,-1] = 1
+        cls.trainY[:,-1] = 1
+
+        # Parameters
+        cls.src_len = len(cls.trainX)
+        cls.tgt_len = len(cls.trainY)
 
 
-params = model_params.TINY_PARAMS
+        assert cls.src_len == cls.tgt_len
 
-input = tl.layers.Input([80, 20], name='input', dtype=tf.int32)
-target = tl.layers.Input([80, 20], name='input', dtype=tf.int32)
+        cls.num_epochs = 1000
+        cls.n_step = cls.src_len // cls.batch_size
 
-model = gan_transformer(params)
-model.train()
-print(model([input, target]))
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
+    def test_basic_simpleSeq2Seq(self):
+        
+
+        model_ = Transformer(TINY_PARAMS)
+        generator = model_.generator
+        discriminator = model_.discriminator
+
+        self.vocab_size = TINY_PARAMS.vocab_size
+        optimizer = tf.optimizers.Adam(learning_rate=0.001)
+
+        cross_entropy = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+
+        def discriminator_loss(real_output, fake_output):
+          real_loss = cross_entropy(tf.ones_like(real_output), real_output)
+          fake_loss = cross_entropy(tf.zeros_like(fake_output), fake_output)
+          total_loss = real_loss + fake_loss
+          return total_loss
+
+        def generator_loss(fake_output, generated_output, targets):
+          discriminator_loss = cross_entropy(tf.ones_like(fake_output), fake_output)
+          logits = metrics.MetricLayer(self.vocab_size)([generated_output, targets])
+          logits, generator_loss = metrics.LossLayer(self.vocab_size, 0.1)([logits, targets])
+          return discriminator_loss + generator_loss, generator_loss
+
+
+
+
+        for epoch in range(self.num_epochs):
+            model_.train()
+            trainX, trainY = shuffle(self.trainX, self.trainY)
+            total_loss_of_d = n_iter = total_loss_of_g = total_loss_of_original = 0
+
+
+
+
+            for X, Y in tqdm(tl.iterate.minibatches(inputs=trainX, targets=trainY, batch_size=self.batch_size,
+                                                    shuffle=False), total=self.n_step,
+                             desc='Epoch[{}/{}]'.format(epoch + 1, self.num_epochs), leave=False):
+
+                with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+
+                    targets = Y
+                    generated_output, fake_output = model_(inputs = X, targets = Y)
+                    real_data = tf.one_hot(targets, TINY_PARAMS.vocab_size, dtype=tf.float32)
+                    real_output = discriminator(real_data)
+
+                    loss_of_g, loss_of_original = generator_loss(fake_output, generated_output, targets)
+                    loss_of_d = discriminator_loss(real_output, fake_output)
+
+                    grad_of_g = gen_tape.gradient(loss_of_g, generator.all_weights)
+                    grad_of_d = disc_tape.gradient(loss_of_d, discriminator.all_weights)
+
+
+                    optimizer.apply_gradients(zip(grad_of_g, generator.all_weights))
+                    optimizer.apply_gradients(zip(grad_of_d, discriminator.all_weights))
+
+
+                total_loss_of_g += loss_of_g
+                total_loss_of_d += loss_of_d
+                total_loss_of_original += loss_of_original
+                n_iter += 1
+
+            model_.eval()
+            test_sample = trainX[0:2, :]
+            model_.eval()
+            prediction = generator(inputs = test_sample)
+            print("Prediction: >>>>>  ", prediction["outputs"], "\n Target: >>>>>  ", trainY[0:2, :], "\n\n")
+
+            print('Epoch [{}/{}]: loss_of_g {:.4f}, loss_of_d {:.4f}, loss_original {:.4f}'.format(epoch + 1, self.num_epochs, total_loss_of_g / n_iter, total_loss_of_d/n_iter, total_loss_of_original/n_iter))
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ task.py
@@ -0,0 +1,129 @@
+import tensorflow as tf
+import tensorlayer as tl
+import numpy as np
+import argparse
+from utils.tokenizer import *
+from models import model_params
+from models.transformer_v2 import Transformer
+from utils.pipeline_dataset import train_input_fn
+from utils import metrics
+from models import optimizer
+from translate_file import translate_file
+from utils import tokenizer
+from compute_bleu import bleu_wrapper
+import time
+
+
+_TARGET_VOCAB_SIZE = 32768  # Number of subtokens in the vocabulary list.
+_TARGET_THRESHOLD = 327  # Accept vocabulary if size is within this threshold
+VOCAB_FILE = "vocab.ende.%d" % _TARGET_VOCAB_SIZE
+
+class CustomSchedule(tf.keras.optimizers.schedules.LearningRateSchedule):
+  def __init__(self, d_model, warmup_steps=5):
+    super(CustomSchedule, self).__init__()
+    
+    self.d_model = d_model
+    self.d_model = tf.cast(self.d_model, tf.float32)
+
+    self.warmup_steps = warmup_steps
+    
+  def __call__(self, step):
+    arg1 = tf.math.rsqrt(step)
+    arg2 = step * (self.warmup_steps ** -1.5)
+    
+    return tf.math.rsqrt(self.d_model) * tf.math.minimum(arg1, arg2)
+
+
+
+
+def train_model(input_params):
+    params = model_params.EXAMPLE_PARAMS
+    dataset = train_input_fn(input_params)
+    subtokenizer = tokenizer.Subtokenizer("data/data/"+VOCAB_FILE)
+    input_file = "data/data/newstest2013.en"
+    output_file = "./output/dev_task.de"
+
+    ref_filename = "data/data/newstest2013.de"
+    trace_path = "task/logging/"
+    num_epochs = 10
+    
+
+    def train_step(inputs, targets):
+        model.train()
+        with tf.GradientTape() as tape:
+            #print(inputs)
+            
+            logits = model(inputs=inputs, targets=targets)
+            logits = metrics.MetricLayer(params.vocab_size)([logits, targets])
+            logits, loss = metrics.LossLayer(params.vocab_size, 0.1)([logits, targets])
+
+            
+        gradients = tape.gradient(loss, model.all_weights)
+        optimizer_.apply_gradients(zip(gradients, model.all_weights))
+        return loss
+
+    
+    model = Transformer(params)
+    # load_weights = tl.files.load_npz(name='./task/model.npz')
+    # tl.files.assign_weights(load_weights, model)
+    learning_rate = CustomSchedule(params.hidden_size, warmup_steps=params.learning_rate_warmup_steps)
+    optimizer_ = optimizer.LazyAdam(learning_rate, beta_1=0.9, beta_2=0.98, epsilon=1e-9)
+    
+    time_ = time.time()
+    for epoch in range(num_epochs):
+        total_loss, n_iter = 0, 0
+        for i, [inputs, targets] in enumerate(dataset):
+            loss = train_step(inputs, targets)
+            with tf.io.gfile.GFile(trace_path+"loss", "ab+") as trace_file:
+                trace_file.write(str(loss.numpy())+'\n')
+            
+            if (i % 100 == 0):
+                print('Batch ID {} at Epoch [{}/{}]: loss {:.4f} using {:.4f}'.format(i, epoch + 1, num_epochs, loss, (time.time()-time_)/100))
+                time_ = time.time()            
+            if ((i+1) % 2000 == 0):
+                tl.files.save_npz(model.all_weights, name='./task/model.npz')
+            if (i == 80000):
+                translate_file(model, subtokenizer, input_file=input_file, output_file=output_file)
+                try:
+                    insensitive_score = bleu_wrapper(ref_filename, output_file, False)
+                    sensitive_score = bleu_wrapper(ref_filename, output_file, True)
+                    with tf.io.gfile.GFile(trace_path+"bleu_insensitive", "ab+") as trace_file:
+                        trace_file.write(str(insensitive_score)+'\n')
+                    with tf.io.gfile.GFile(trace_path+"bleu_sensitive", "ab+") as trace_file:
+                        trace_file.write(str(sensitive_score)+'\n')
+                except:
+                    print("An exception occurred")
+         
+            total_loss += loss
+            n_iter += 1
+
+        # printing average loss after every epoch
+        print('Epoch [{}/{}]: loss {:.4f}'.format(epoch + 1, num_epochs, total_loss / n_iter))
+        # save model weights after every epoch
+        tl.files.save_npz(model.all_weights, name='./task/model.npz')
+
+        # translate the evaluation file and calculate bleu scores
+        # translate_file(model, subtokenizer, input_file=input_file, output_file=output_file)
+        # insensitive_score = bleu_wrapper(ref_filename, output_file, False)
+        # sensitive_score = bleu_wrapper(ref_filename, output_file, True)
+        # with tf.io.gfile.GFile(trace_path+"bleu_insensitive", "ab+") as trace_file:
+        #     trace_file.write(str(insensitive_score)+'\n')
+        # with tf.io.gfile.GFile(trace_path+"bleu_sensitive", "ab+") as trace_file:
+        #     trace_file.write(str(sensitive_score)+'\n')   
+
+
+
+
+
+if __name__ == '__main__':
+
+    params = {}
+    params["batch_size"] = 2048
+    params["max_length"] = 256
+    params["num_parallel_calls"] = 1
+    params["repeat_dataset"] = 1
+    params["static_batch"] = False
+    params["num_gpus"] = 1
+    params["use_synthetic_data"] = False
+    params["data_dir"] = './data/data/wmt32k-train*'
+    train_model(params)
