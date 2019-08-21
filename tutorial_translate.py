85c135b96adc00797d717608fedf37d40a1d69d1
Lingjun Liu <lingjunliu@LingjundeMacBook-Pro.local>
2019-08-21 11:19:26 +0100

    dfa

--- tutorial_translate.py
+++ tutorial_translate.py
@@ -171,7 +171,7 @@ if __name__ == "__main__":
   if (len(sys.argv) > 1 and sys.argv[1] == "dw"):
     params = model_params_dw.EXAMPLE_PARAMS
     # params.beam_size = 1
-    model = Transformer_ngram(params)
+    model = Transformer_DW(params)
     load_weights = tl.files.load_npz(name='./checkpoints_dw/model.npz')
     tl.files.assign_weights(load_weights, model)
     input_file = "./data/data/newstest2014.en"
